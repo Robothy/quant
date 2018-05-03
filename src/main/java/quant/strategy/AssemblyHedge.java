d4ab9b5797468c3ddff8b05dbcf7e8a0604c2829
Fuxiang.Luo <robothyluo@gmail.com>
2018-05-03 18:35:31 +0800

    添加组合对冲交易代码

--- src/main/java/quant/strategy/AssemblyHedge.java
+++ src/main/java/quant/strategy/AssemblyHedge.java
@@ -1,11 +1,35 @@
 package quant.strategy;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+
+import org.apache.logging.log4j.Logger;
 
 public class AssemblyHedge implements Strategy {
 
+	private static final Logger logger = LogManager.getLogger(AssemblyHedge.class);
+	
+	private Map<String, List<String>> candidateCurrencyPairs = new HashMap<String, List<String>>();
+	
+	private Boolean init(Map<String, Object> parameters){
+		Object candidateCurrencyPairs = parameters.get("candidateCurrencyPairs");
+		if(null == candidateCurrencyPairs){
+			logger.fatal("未指明备选交易对。");
+			return false;
+		}
+		if(candidateCurrencyPairs instanceof HashMap){
+			this.candidateCurrencyPairs = (Map<String, List<String>>) candidateCurrencyPairs;
+		}
+		return true;
+	}
+	
+	
 	public void run(Map<String, Object> parameters) {
-		// TODO Auto-generated method stub
+		
+		
+		
 		
 	}
 
