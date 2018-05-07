63b4503cde8f9dc555fdc7c50fc6df526f0ab09a
Fuxiang.Luo <robothyluo@gmail.com>
2018-05-07 15:41:34 +0800

    添加实体类AssemblyHedgeOrder

--- src/main/resources/genTable.sql
+++ src/main/resources/genTable.sql
@@ -16,3 +16,21 @@ create table LIVE_ORDER_PAIR(
 	SELL_ORDER_QUANTITY numeric(20, 10),
 	SELL_ORDER_STATUS varchar(15)	
 )
+
+/**
+ * 组合对冲策略订单记录表
+ **/
+create table ASSEMBLY_HEDGE_ORDER(
+	DATA_ID char(32) primary key,	-- 数据ID
+	HEDGE_ID char(32),				-- 对冲ID
+	PLANTFROM varchar(30),			-- 平台
+	CURRENCY varchar(15),			-- 币种
+	ORDER_ID varchar(32),			-- 挂单ID
+	ORDER_SIDE varchar(20),			-- 挂单方向
+	ORDER_STATUS varchar(15),		-- 挂单状态
+	ORDER_PRICE numeric(20, 10),	-- 挂单价格
+	ORDER_QUANTITY numeric(20, 10),	-- 挂单数量
+	TRANS_PRICE	numeric(20, 10),	-- 挂单成交价格
+	CREATE_TIMESTAMP varchar(13),	-- 挂单创建时间
+	MODIFY_TIMESTAMP varchar(13)	-- 挂单修改时间
+)
