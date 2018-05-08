c58059a21e5ac83b66d59582650fb05c6b935f43
Fuxiang.Luo <robothyluo@gmail.com>
2018-05-08 16:48:16 +0800

    完善策略

--- src/main/java/quant/strategy/AssemblyHedge.java
+++ src/main/java/quant/strategy/AssemblyHedge.java
@@ -1,20 +1,21 @@
 package quant.strategy;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.logging.log4j.LogManager;
-
 import org.apache.logging.log4j.Logger;
-import org.hibernate.id.GUIDGenerator;
-import org.hibernate.id.UUIDGenerator;
 
 import exunion.exchange.Exchange;
 import exunion.metaobjects.Depth;
+import exunion.metaobjects.Order;
+import exunion.metaobjects.OrderStatus;
+import quant.dao.CommonDao;
 import quant.entity.AssemblyHedgeOrder;
 import quant.exchange.EndExchangeFactory;
 
@@ -56,17 +57,33 @@ public class AssemblyHedge implements Strategy {
 	
 	private static final Logger logger = LogManager.getLogger(AssemblyHedge.class);
 	
-	// 组合对冲币种对
+	private final CommonDao commonDao = new CommonDao();
+	
+	// 组合对冲币种对，即参与对冲组合的币种对，属性包括平台，币种对
 	private List<HedgeCurrencyPair> hedgeCurrencyPairs = null;
 	
+	/**
+	 * 获取组合对冲币种对
+	 * <p>
+	 * 组合对冲币种对，即参与对冲组合的币种对，属性包括平台，币种对
+	 * @return 组合对冲币种对
+	 */
+	public List<HedgeCurrencyPair> getHedgeCurrencyPairs() {
+		return hedgeCurrencyPairs;
+	}
+
+	/**
+	 * 获取组合对冲币种对
+	 * <p>
+	 * 组合对冲币种对，即参与对冲组合的币种对，属性包括平台，币种对
+	 * @param hedgeCurrencyPairs 组合对冲币种对
+	 */
+	public void setHedgeCurrencyPairs(List<HedgeCurrencyPair> hedgeCurrencyPairs) {
+		this.hedgeCurrencyPairs = hedgeCurrencyPairs;
+	}
+
 	// 交易所实例
 	private Map<String, Exchange> exchanges = null;
-	
-	//API操作失败休息时间， （单位：毫秒），默认 1000ms
-	private Long failedSleepTime = 1000L;
-	
-	//操作周期, 默认10毫秒
-	private Long cycle = 10L;
 	
 	/**
 	 * @return 当前策略实例使用到的交易所实例
@@ -74,7 +91,10 @@ public class AssemblyHedge implements Strategy {
 	public Map<String, Exchange> getExchanges() {
 		return exchanges;
 	}
-
+	
+	//API操作失败休息时间， （单位：毫秒），默认 1000ms
+	private Long failedSleepTime = 1000L;
+	
 	/**
 	 * @return 操作失败时休息时间(单位：毫秒)
 	 */
@@ -90,7 +110,10 @@ public class AssemblyHedge implements Strategy {
 		this.failedSleepTime = failedSleepTime;
 		return this;
 	}
-
+	
+	//操作周期, 默认10毫秒
+	private Long cycle = 10L;
+	
 	/**
 	 * @return 每轮操作的时间（单位：毫秒）
 	 */
@@ -106,6 +129,58 @@ public class AssemblyHedge implements Strategy {
 		this.cycle = cycle;
 		return this;
 	}
+	
+	
+	/**
+	 *  市场有效时间(单位：毫秒)，默认值 500，表示市场行情有效时间，
+	 *  即在获取行情数据时，交易所必须在此时间之内返回行情数据，否则认为获取到的数据无效
+	 */
+	private Long marketAvailableDuration = 500L;
+	
+	/**
+	 * 获取市场有效时间
+	 * <p>
+	 * 市场有效时间(单位：毫秒)，默认值 500，表示市场行情有效时间，
+	 * 即在获取行情数据时，交易所必须在此时间之内返回行情数据，否则认为获取到的数据无效
+	 * @return 市场有效时间
+	 */
+	public Long getMarketAvailableDuration() {
+		return marketAvailableDuration;
+	}
+
+	/**
+	 * 设置市场有效时间
+	 * <p>
+	 * 市场有效时间(单位：毫秒)，默认值 500，表示市场行情有效时间，
+	 * 即在获取行情数据时，交易所必须在此时间之内返回行情数据，否则认为获取到的数据无效
+	 * @param marketAvailableDuration 市场有效时间，默认值 500
+	 */
+	public void setMarketAvailableDuration(Long marketAvailableDuration) {
+		this.marketAvailableDuration = marketAvailableDuration;
+	}
+
+	
+	// 最大计划订单数
+	private Integer maxPlanOrderNumber = 5;
+	
+	/**
+	 * 默认值为 5， 当达到最大订单数时，不再创建新的对冲交易订单组合
+	 * @return 最大计划订单数
+	 */
+	public Integer getMaxPlanOrderNumber() {
+		return maxPlanOrderNumber;
+	}
+
+	/**
+	 * 
+	 * 默认值为 5， 当达到最大订单数时，不再创建新的对冲交易订单数
+	 * @param maxPlanOrderNumber 最大计划订单数
+	 */
+	public AssemblyHedge setMaxPlanOrderNumber(Integer maxPlanOrderNumber) {
+		this.maxPlanOrderNumber = maxPlanOrderNumber;
+		return this;
+	}
+	
 	
 	/*****************************************************************
 	 * 
@@ -169,14 +244,24 @@ public class AssemblyHedge implements Strategy {
 			}).start();
 		}
 		
-		// 前面在并发获取深度信息的过程中，若没有获取到深度，则不将深度信息添加到 result 中
-		// 所以这里比较待获取的深度信息的交易对的数量和已经获取到深度信息的交易对的数量就可以知道是否存在某个交易对没有获取到深度信息
-		// 若存在没有获取到深度信息的交易对，则返回 null
-		if(result.size() != hedgeCurrencyPairs.size()){
-			return null;
+		// 判断是否获取到各币种的深度信息
+		// 每10ms查询一次，直到达到最大次数或者该获取的行情信息都获取到了则退出循环
+		Long maxCycleTimes = (marketAvailableDuration - (marketAvailableDuration % 10)) / 10;
+		Long i = 0L;
+		Boolean isGetAllDepth = false;
+		while(i++ < maxCycleTimes){
+			delay(10L);
+			if(result.size() == hedgeCurrencyPairs.size()){
+				isGetAllDepth = true;
+				break;
+			}
 		}
 		
-		return result;
+		// 获取到了所有的行情信息
+		if(isGetAllDepth){
+			return result;			
+		}
+		return null;
 	}
 	
 	/**
@@ -190,6 +275,69 @@ public class AssemblyHedge implements Strategy {
 		return assemblyHedgeOrders;
 	}
 	
+	/**
+	 * 根据对冲订单组合挂单
+	 * @param assemblyHedgeOrders 对冲组合订单
+	 * @return 订单信息
+	 */
+	private List<AssemblyHedgeOrder> order(List<AssemblyHedgeOrder> assemblyHedgeOrders){
+		final List<AssemblyHedgeOrder> hedgeOrders = Collections.synchronizedList(new ArrayList<AssemblyHedgeOrder>());
+		
+		// 并发下订单
+		for(final AssemblyHedgeOrder assemblyHedgeOrder: assemblyHedgeOrders){
+			
+			new Thread(new Runnable() {
+				public void run() {
+					AssemblyHedgeOrder hedgeOrder = order(assemblyHedgeOrder);
+					hedgeOrders.add(hedgeOrder);
+				}
+			}).start();
+			
+		}
+
+		// 等待下单完成
+		while(assemblyHedgeOrders.size()!=hedgeOrders.size()){
+			delay(10L);
+		}
+		
+		// 判断是否所有的订单都下单失败了，即所有订单的状态都是 PLAN
+		// 若对冲组合中存在下单成功的订单，则失败的订单的状态变为 PLAN，日后伺机下单
+		Boolean allOrdersArePlan = true;
+		for(AssemblyHedgeOrder hedgeOrder : hedgeOrders){
+			if(!"PLAN".equals(hedgeOrder.getOrderStatus())){
+				allOrdersArePlan = false;				
+				break;
+			}
+		}
+		// 若所有的订单都挂单失败了，则表示此轮对冲组合无效，则直接清空此轮的订单
+		if(allOrdersArePlan){
+			hedgeOrders.clear();
+		}
+		
+		return hedgeOrders;
+	}
+	
+	/**
+	 * 根据组合对冲订单的成员信息下订单
+	 * @param assemblyHedgeOrder 组合对冲订单对
+	 * @return 订单信息
+	 */
+	private AssemblyHedgeOrder order(final AssemblyHedgeOrder assemblyHedgeOrder){
+		String exchangeName = assemblyHedgeOrder.getPlantfrom();
+		Exchange exchange = exchanges.get(exchangeName);
+		String side = assemblyHedgeOrder.getOrderSide();
+		String currency = assemblyHedgeOrder.getCurrency();
+		BigDecimal quantity = assemblyHedgeOrder.getOrderQuantity();
+		BigDecimal price = assemblyHedgeOrder.getOrderPrice();
+		Order order = exchange.order(side, currency, quantity, price);
+		if(null != order){
+			assemblyHedgeOrder.setOrderId(order.getOrderId());
+			assemblyHedgeOrder.setOrderStatus(OrderStatus.NEW);
+		}else{
+			assemblyHedgeOrder.setOrderStatus("PLAN");
+		}
+		return assemblyHedgeOrder;
+	}
 	
 	/**
 	 * 延时函数
@@ -220,7 +368,6 @@ public class AssemblyHedge implements Strategy {
 		}
 		logger.info("初始化完成 ...");
 		
-		
 		// 开始执行策略
 		while(true){
 			logger.debug("新一轮。");
@@ -233,8 +380,15 @@ public class AssemblyHedge implements Strategy {
 				continue;
 			}
 			
+			// 计算组合单
 			List<AssemblyHedgeOrder> assemblyHedgeOrders = this.CalculateAssemblyHedgeOrder(hedgeCurrencyPairDepth);
-			
+			if(0 == assemblyHedgeOrders.size()){
+				logger.info("没有组合对冲交易机会。");
+			}else{
+				// 挂组合单
+				List<AssemblyHedgeOrder> hedgeOrders = order(assemblyHedgeOrders);
+				this.commonDao.saveOrUpdate(hedgeOrders);
+			}
 			
 		}
 		
@@ -243,8 +397,13 @@ public class AssemblyHedge implements Strategy {
 	
 	
 	public static void main(String[] args){
-		Exchange exchange = EndExchangeFactory.newInstance("zb.com");
-		System.out.println(exchange.getDepth("HSR_QC"));
+		
+		List<String> l = Collections.synchronizedList(new ArrayList<String>());
+		l.add(null);
+		l.add(null);
+		l.add("1");
+		System.out.println(l.contains(null));
+		
 	}
 	
 }
